5114d09d028da7dfc0547fb5109178cd2faf4e63
Deivison <dguarines@gmail.com>
2018-08-02 13:27:59 -0300

    Part 2

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -25,7 +25,45 @@ class BmiState extends State<Bmi>{
             padding: const EdgeInsets.all(2.0),
             children: <Widget>[
               new Image.asset('images/bmilogo.png', height:85.0, width: 75.0),
-              
+              new Container(
+                margin: const EdgeInsets.all(3.0),
+                height: 245.0,
+                width: 290.0,
+                color: Colors.grey.shade300,
+                child: new Column(
+                  children: <Widget>[
+                    new TextField(
+                      controller: null,
+                      keyboardType: TextInputType.number,
+                      decoration: new InputDecoration(
+                        labelText: 'Age',
+                        hintText: 'e.g: 34',
+                        icon: new Icon(Icons.person_outline)
+                      ),
+                    ),
+
+                    new TextField(
+                      controller: null,
+                      keyboardType: TextInputType.number,
+                      decoration: new InputDecoration(
+                          labelText: 'Height in feet',
+                          hintText: 'e.g: 6.5',
+                          icon: new Icon(Icons.insert_chart)
+                      )
+                    ),
+
+                    new TextField(
+                        controller: null,
+                        keyboardType: TextInputType.number,
+                        decoration: new InputDecoration(
+                            labelText: 'Weight in lbs',
+                            hintText: 'e.g: 180',
+                            icon: new Icon(Icons.person)
+                        )
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         )
