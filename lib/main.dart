2720982585e84a4cbd7e01f50318df434faf6faa
Deivison <dguarines@gmail.com>
2018-08-13 13:16:27 -0300

    Json parse part 2

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,54 @@ void main() async  {
           backgroundColor: Colors.orangeAccent,
         ),
         body: new Center(
-          child: new Text(_body),
+          child: new ListView.builder(
+            itemCount: _data.length,
+            padding: const EdgeInsets.all(14.5),
+            itemBuilder: (BuildContext context, int position){
+                return new Column(
+                  children: <Widget>[
+                    new Divider(height: 5.5),
+                    new ListTile(
+                      title: Text("${_data[position]['title']}",
+                          style: new TextStyle(fontSize: 17.9)),
+                      subtitle: Text("${_data[position]['body']}",
+                          style: new TextStyle(fontSize: 13.9,
+                          color: Colors.grey,
+                          fontStyle: FontStyle.italic)),
+                      leading: new CircleAvatar(
+                        backgroundColor: Colors.greenAccent,
+                        child: Text("${_data[position]['body'][0]}",
+                        style: new TextStyle(fontSize: 13.4,
+                        color: Colors.orangeAccent),),
+                      ),
+                      onTap: () => _showOnTapMessage(context, _data[position]['body']),
+                    )
+                  ],
+                );
+            }),
+          ),
         ),
       ),
-  ));
+  );
+}
+
+void _showOnTapMessage(BuildContext context, String message){
+  var alert = new AlertDialog(
+    title: Text("My App"),
+    content: Text(message),
+    actions: <Widget>[
+      FlatButton(
+        child: Text("OK"),
+        onPressed: () {
+          Navigator.pop(context);
+        },)
+    ],
+  );
+      
+  //showDialog(context: context, child: alert);
+  showDialog(context: context, builder: (context) {
+    return alert;
+  });
 }
 
 Future<List> getJson() async {
