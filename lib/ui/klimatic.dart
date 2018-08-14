7f041b972ee532cc5c49ed61adcb622647db92fc
Deivison <dguarines@gmail.com>
2018-08-14 14:07:29 -0300

    Klimatic part 2

--- lib/ui/klimatic.dart
+++ lib/ui/klimatic.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../util/utils.dart' as util;
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -15,11 +16,79 @@ class _KlimaticState extends State<Klimatic> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Klimatic'),
-        backgroundColor: Colors.white,
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
         actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.menu),
+                onPressed: () => debugPrint("Hey")
+            )
+        ],
+      ),
+      body: new Stack(
+        children: <Widget>[
 
+          new Center(
+            child: new Image.asset('images/umbrella.png',
+                width: 490.0,
+                height: 1200.0,
+                fit: BoxFit.fill ),
+          ),
+
+          new Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
+            child: new Text('Spokane',
+                style: cityStyle(),),
+          ),
+
+          new Container(
+            alignment: Alignment.center,
+            child: new Image.asset('images/light_rain.png'),
+          ),
+
+          new Container(
+            margin: const EdgeInsets.fromLTRB(30.0, 290.0, 0.0, 0.0),
+            child: updateTempWidget("Beira"),
+          ),
         ],
       ),
     );
   }
+
+  Future<Map> getWeather(String apiId, String city) async {
+    String apiUrl =
+        'http://api.openweathermap.org/data/2.5/weather?q=$city&appid='
+        '${util.apiId}&units=imperial';
+
+    http.Response response = await http.get(apiUrl);
+
+    return json.decode(response.body);
+  }
+
+  Widget updateTempWidget(String city) {
+    return new FutureBuilder(
+        future: getWeather(util.apiId, city) ,
+        builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+          //Where we get all of the json data, we setup widgets
+        if()
+     });
+  }
+}
+
+TextStyle cityStyle(){
+  return new TextStyle(
+    color: Colors.white,
+    fontSize: 22.9,
+    fontStyle: FontStyle.italic
+  );
+}
+
+TextStyle tempStyle() {
+  return new TextStyle(
+    color: Colors.white,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w500,
+    fontSize: 49.9
+  );
 }
---
+++ lib/util/utils.dart
@@ -0,0 +1,3 @@
+
+final apiId = "ed60fcfbd110ee65c7150605ea8aceea";
+final defaultCity = "Spokane";
