d1ab2c276aaca83d3d4e912357634792bd15b5c0
Deivison <dguarines@gmail.com>
2018-08-16 13:31:48 -0300

    Final

--- lib/ui/klimatic.dart
+++ lib/ui/klimatic.dart
@@ -1,16 +1,41 @@
-import 'package:flutter/material.dart';
-import '../util/utils.dart' as util;
 import 'dart:async';
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../util/utils.dart' as util;
 
 class Klimatic extends StatefulWidget {
   @override
-  _KlimaticState createState() => _KlimaticState();
+  _KlimaticState createState() => new _KlimaticState();
 }
 
 class _KlimaticState extends State<Klimatic> {
+
+  String _cityEntered;
+
+  Future _goToNextScreen(BuildContext context) async {
+    Map results = await Navigator
+        .of(context)
+        .push(new MaterialPageRoute<Map>(builder: (BuildContext context) { //change to Map instead of dynamic for this to work
+      return new ChangeCity();
+    }));
+
+    if ( results != null && results.containsKey('enter')) {
+      _cityEntered = results['enter'];
+
+//      debugPrint("From First screen" + results['enter'].toString());
+
+
+    }
+  }
+
+  void showStuff() async {
+    Map data = await getWeather(util.appId, util.defaultCity);
+    print(data.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -19,47 +44,53 @@ class _KlimaticState extends State<Klimatic> {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
         actions: <Widget>[
-            new IconButton(
-                icon: new Icon(Icons.menu),
-                onPressed: () => debugPrint("Hey")
-            )
+          new IconButton(
+              icon: new Icon(Icons.menu),
+              onPressed: () {
+                _goToNextScreen(context);
+              })
         ],
       ),
       body: new Stack(
         children: <Widget>[
-
           new Center(
-            child: new Image.asset('images/umbrella.png',
-                width: 490.0,
-                height: 1200.0,
-                fit: BoxFit.fill ),
+            child: new Image.asset(
+              'images/umbrella.png',
+              width: 490.0,
+              height: 1200.0,
+              fit: BoxFit.fill,
+            ),
           ),
-
           new Container(
             alignment: Alignment.topRight,
             margin: const EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
-            child: new Text('Spokane',
-                style: cityStyle(),),
+            child: new Text(
+              '${_cityEntered == null ? util.defaultCity : _cityEntered}',
+              style: cityStyle(),
+            ),
           ),
 
           new Container(
             alignment: Alignment.center,
             child: new Image.asset('images/light_rain.png'),
           ),
+          updateTempWidget(_cityEntered)
 
-          new Container(
-            margin: const EdgeInsets.fromLTRB(30.0, 290.0, 0.0, 0.0),
-            child: updateTempWidget("natal"),
-          ),
+          //Container which will have our weather data
+//          new Container(
+//            //margin: const EdgeInsets.fromLTRB(30.0, 310.0, 0.0, 0.0),
+//
+//            child: ,
+//          )
         ],
       ),
     );
   }
 
-  Future<Map> getWeather(String apiId, String city) async {
+  Future<Map> getWeather(String appId, String city) async {
     String apiUrl =
         'http://api.openweathermap.org/data/2.5/weather?q=$city&appid='
-        '${util.apiId}&units=imperial';
+        '${util.appId}&units=imperial';
 
     http.Response response = await http.get(apiUrl);
 
@@ -68,48 +99,115 @@ class _KlimaticState extends State<Klimatic> {
 
   Widget updateTempWidget(String city) {
     return new FutureBuilder(
-        future: getWeather(util.apiId, city) ,
+        future: getWeather(util.appId, city == null ? util.defaultCity : city),
         builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
-          //Where we get all of the json data, we setup widgets
-        if(snapshot.hasData){
-          Map content = snapshot.data;
-          return new Container(
-            child: new Column(
-              children: <Widget>[
-                new ListTile(
-                  title: new Text(content['main']['temp'].toString(),
-                  style: new TextStyle(
-                    fontStyle: FontStyle.normal,
-                    fontSize: 49.9,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500
-                  ),),
-                )
-              ],
-            )
-          );
-        }else {
-          return new Container(
+          //where we get all of the json data, we setup widgets etc.
+          if (snapshot.hasData) {
+            Map content = snapshot.data;
+            return new Container(
+              margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new ListTile(
+                    title: new Text(
+                      content['main']['temp'].toString() +" F",
+                      style: new TextStyle(
+                          fontStyle: FontStyle.normal,
+                          fontSize: 49.9,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: new ListTile(
+                      title: new Text(
+                        "Humidity: ${content['main']['humidity'].toString()}\n"
+                            "Min: ${content['main']['temp_min'].toString()} F\n"
+                            "Max: ${content['main']['temp_max'].toString()} F ",
 
-          );
-        }
-     });
+                        style: extraData(),
+
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          } else {
+            return new Container();
+          }
+        });
   }
 }
 
-TextStyle cityStyle(){
-  return new TextStyle(
-    color: Colors.white,
-    fontSize: 22.9,
-    fontStyle: FontStyle.italic
-  );
+class ChangeCity extends StatelessWidget {
+
+  var _cityFieldController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        title: new Text('Change City'),
+        centerTitle: true,
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset(
+              'images/white_snow.png',
+              width: 490.0,
+              height: 1200.0,
+              fit: BoxFit.fill,
+            ),
+          ),
+
+          new ListView(
+            children: <Widget>[
+              new ListTile(
+                title: new TextField(
+                  decoration: new InputDecoration(
+                    hintText: 'Enter City',
+                  ),
+                  controller: _cityFieldController,
+                  keyboardType: TextInputType.text,
+                ),
+
+              ),
+              new ListTile(
+                title: new FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context, {
+                        'enter': _cityFieldController.text
+                      });
+                    },
+                    textColor: Colors.white70,
+                    color: Colors.redAccent,
+                    child: new Text('Get Weather')),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
 
+TextStyle cityStyle() {
+  return new TextStyle(
+      color: Colors.white, fontSize: 22.9, fontStyle: FontStyle.italic);
+}
+
+TextStyle extraData() {
+  return new TextStyle(
+      color: Colors.white70,
+      fontStyle: FontStyle.normal,
+      fontSize: 17.0);
+
+}
 TextStyle tempStyle() {
   return new TextStyle(
-    color: Colors.white,
-    fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w500,
-    fontSize: 49.9
-  );
+      color: Colors.white,
+      fontStyle: FontStyle.normal,
+      fontWeight: FontWeight.w500,
+      fontSize: 49.9);
 }
--- lib/util/utils.dart
+++ lib/util/utils.dart
@@ -1,3 +1,3 @@
 
-final apiId = "ed60fcfbd110ee65c7150605ea8aceea";
+final appId = "ed60fcfbd110ee65c7150605ea8aceea";
 final defaultCity = "Spokane";
